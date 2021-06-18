2f2d4105acf5b55e3de92c12ebd44ca2a9a9b637
Meraj Hussain Syed <merajhussain1@gmail.com>
2021-06-18 08:23:25 +0530

    perf Comparison between std::stack and mystack

--- stack.cxx
+++ stack.cxx
@@ -1,95 +1,119 @@
 #include<iostream>
 #include<cstring>
+#include<stack>
+#include<chrono>
 using namespace std;
 typedef unsigned long long ull;
-template<class T>
-class   stack
+namespace MyStack
 {
-	T* elems;
-	ull sz;
-	ull tos;
-
-public:
-
-	stack()
+	template<class T>
+	class   stack
 	{
-		sz = 100;
-		elems = new T[sz];
-		tos = 0;
-	}
-	bool push(T t)
-	{
-		if (tos + 1 == sz)
+		T* elems;
+		ull sz;
+		ull tos;
+
+	public:
+
+		stack()
 		{
+			sz = 10000;
+			elems = new T[sz];
+			tos = 0;
+		}
+		bool push(T t)
+		{
+			if (tos + 1 == sz)
+			{
 
-			resize(10000);
+				resize(10000);
+				elems[tos] = t;
+				tos++;
+				return true;
+			}
+
+			if (tos == -1) tos++;
+
 			elems[tos] = t;
 			tos++;
+
 			return true;
 		}
-
-		if (tos == -1) tos++;
-
-		elems[tos] = t;
-		tos++;
-
-		return true;
-	}
-	bool pop()
-	{
-		if (tos == -1)
+		bool pop()
 		{
-			cout << "stack underflow" << endl;
+			if (tos == -1)
+			{
+				cout << "stack underflow" << endl;
+				return false;
+			}
+			//cout << "pop:" << elems[tos] << endl;
+
+			tos--;
+			return true;
+
+		}
+		T top()
+		{
+
+			return elems[tos - 1];
+			elems = nullptr;
+		}
+
+		int size()
+		{
+			return tos;
+		}
+		void print()
+		{
+			ull	 k = 0;
+			for (ull i = 0; i < size(); i++)
+			{
+				cout << elems[i] << " ";
+				k++;
+			}
+			cout << endl;
+			cout << "total elements:" << k << endl;
+			cout << "actual size :" << size() << endl;
+		}
+		void resize(int size)
+		{
+
+			T *el = new T[sz + size];
+			memcpy(el, elems, sizeof(T)*tos);
+			sz += size;
+			delete[] elems;
+			elems = el;
+		}
+		~stack()
+		{
+			delete[] elems;
+		}
+
+		void destroy()
+		{
+			delete[] elems;
+			sz = 100;
+			elems = new T[sz];
+			tos = 0;
+
+		}
+
+		bool isEmpty()
+		{
+			if (tos == -1) return true;
+
 			return false;
 		}
-		cout << "pop:" << elems[tos] << endl;
+	};
 
-		tos--;
-		return true;
-
-	}
-	T top()
-	{
-
-		return elems[tos - 1];
-		elems = nullptr;
-	}
-
-	int size()
-	{
-		return tos;
-	}
-	void print()
-	{
-		ull	 k = 0;
-		for (ull i = 0; i < size(); i++)
-		{
-			cout << elems[i] << " ";
-			k++;
-		}
-		cout << endl;
-		cout << "total elements:" << k << endl;
-		cout << "actual size :" << size() << endl;
-	}
-	void resize(int size)
-	{
-
-		T *el = new T[sz + size];
-		memcpy(el, elems, sizeof(T)*tos);
-		sz += size;
-		delete[] elems;
-		elems = el;
-	}
-	~stack()
-	{
-		delete[] elems;
-	}
-};
+}
 
 // driver program
 int main()
 {
-	stack<int> st;
+
+	auto start = std::chrono::steady_clock::now();
+	MyStack::stack<int> st;
 	for (int i = 1; i <= 99; i++)
 	{
 		st.push(i);
@@ -103,23 +127,69 @@ int main()
 	{
 		st.push(i);
 	}
-	st.print();
+	//st.print();
 
 	cout << st.top() << endl;
 	cout << st.size() << endl;
 
 	st.pop();
-	st.print();
+	//st.print();
 	cout << st.size() << endl;
 	cout << st.size() << endl;
 
-	while (st.pop())
+	while (!st.isEmpty())
 	{
-
+		st.pop();
 	}
 	st.push(10);
 	cout << st.size() << endl;
 	cout << st.top() << endl;
-	st.print();
-	return 0;
+	//st.print();
+	st.destroy();
+
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
+
+
+	cout <<"=============std stack=====================" << endl;
+	auto start1 = std::chrono::steady_clock::now();
+	std::stack<int> stdst;
+	for (int i = 1; i <= 99; i++)
+	{
+		stdst.push(i);
+	}
+	cout << stdst.top() << endl;
+	cout << st.size() << endl;
+	stdst.push(100);
+	stdst.push(101);
+	stdst.push(102);
+	for (int i = 103; i <= 20000; i++)
+	{
+		stdst.push(i);
+	}
+	 
+
+	cout << stdst.top() << endl;
+	cout << stdst.size() << endl;
+
+	stdst.pop();
+	 
+	cout << stdst.size() << endl;
+	cout << stdst.size() << endl;
+
+	while (!stdst.empty())
+	{
+		stdst.pop();
+	}
+	stdst.push(10);
+	cout << stdst.size() << endl;
+	cout << stdst.top() << endl;
+	auto end1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsed_seconds1 = end1 - start1;
+	std::cout << "elapsed time: " << elapsed_seconds1.count() << "s\n";
+
+	 
+	 
 }
