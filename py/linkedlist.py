f4a4c79047ec9f13b4741a2e4274a4079b249192
Meraj Syed <merajhussain1@gmail.com>
2026-03-11 23:29:40 +0530

    py/linkedlist.py

--- py/linkedlist.py
+++ py/linkedlist.py
@@ -38,6 +38,36 @@ class LinkedList:
             curr = nextn
         self.head = prev
 
+    def swapNodes(self,key1,key2):
+        if key1 == key2:
+            return
+        c1 = self.head
+        p1=None
+        c2 = self.head
+        p2=None
+        while c1 and c1.data!=key1:
+            p1=c1
+            c1=c1.next
+        #print(c1.data)
+
+        while c2 and c2.data != key2:
+            p2 = c2
+            c2 = c2.next
+
+        if p1:
+            p1.next = c2
+        else:
+            self.head = c2
+
+        if p2:
+            p2.next = c1
+        else:
+            self.head = c1
+
+        c1.next ,c2.next = c2.next,c1.next
+        #print(c2.data)
+
+
 ll = LinkedList()
 ll.addData(1)
 ll.addData(2)
@@ -46,4 +76,6 @@ ll.addData(4)
 ll.addData(5)
 ll.printLL()
 ll.reverse()
+ll.printLL()
+ll.swapNodes(5,4)
 ll.printLL()
