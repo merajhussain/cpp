e73c11c6b7c4f151e9ca4d8417d28fb71d239b18
Meraj Syed <merajhussain1@gmail.com>
2026-03-12 00:20:00 +0530

    Added unit test for linkedlist

---
+++ py/lltest.py
@@ -0,0 +1,44 @@
+from linkedlist import *
+
+
+def test_createLinkList():
+    ll = LinkedList()
+    ll.addData(1)
+    ll.addData(2)
+    ll.addData(3)
+    ll.addData(4)
+    ll.addData(5)
+    ls = "1->2->3->4->5"
+    assert  ls == ll.to_String()
+
+def test_reverseLinkedList():
+    ll = LinkedList()
+    ll.addData(1)
+    ll.addData(2)
+    ll.addData(3)
+
+    ls ="3->2->1"
+    ll.reverse()
+    assert ls == ll.to_String()
+
+def test_swapNodesList():
+    ll = LinkedList()
+    ll.addData(1)
+    ll.addData(2)
+    ll.addData(3)
+    ll.addData(4)
+    ll.addData(5)
+    ll.swapNodes(2,5)
+    ls = "1->5->3->4->2"
+    assert ls == ll.to_String()
+
+def test_swapNodesListHead():
+    ll = LinkedList()
+    ll.addData(1)
+    ll.addData(2)
+    ll.addData(3)
+    ll.addData(4)
+    ll.addData(5)
+    ll.swapNodes(1,5)
+    ls = "5->2->3->4->1"
+    assert ls == ll.to_String()
