52747b36afbdad4d9b595076de14d4f3399c92e7
Meraj Syed <merajhussain1@gmail.com>
2026-03-19 22:34:51 +0530

    clean up

--- py/targetsum.py
+++ py/targetsum.py
@@ -15,17 +15,5 @@ def targetsum(arr,target):
     return False
 
 
-print(targetsum([2,7,11,15], 9))
-print(targetsum([3,2,4], 6))
-print(targetsum([3,3], 6))
-print(targetsum([1,2,3,4,5], 5))
-print(targetsum([1,1,1,2,2], 3))
-print(targetsum([0,0], 0))
-print(targetsum([0,1,2,3], 3))
-print(targetsum([-1,-2,-3,-4,-5], -8))
-print(targetsum([-1,0,1,2,-1,-4], 0))
-print(targetsum([10,20,30], 100))
-print(targetsum([1], 2))
-print(targetsum([], 0))
 
 
