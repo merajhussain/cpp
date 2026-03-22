1b601997fbb507d11ffea31cc90acffde259c730
Meraj Syed <merajhussain1@gmail.com>
2026-03-23 00:03:40 +0530

    using meaningful names

--- py/max_profit.py
+++ py/max_profit.py
@@ -1,12 +1,12 @@
 def max_profit(prices):
     if len(prices)==0:
         return 0
-    mp=0
-    mbp=prices[0]
+    maxprofit=0
+    minimumbuyingprice=prices[0]
     for i in range (1,len(prices)):
-        if prices[i]<mbp:
-            mbp = prices[i]
-        mp = max(mp,prices[i]-mbp)
-    return mp
+        if prices[i]<minimumbuyingprice:
+            minimumbuyingprice = prices[i]
+        maxprofit = max(maxprofit,prices[i]-minimumbuyingprice)
+    return maxprofit
 
 
