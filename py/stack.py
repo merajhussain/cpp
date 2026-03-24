6daff1cd976a549913ef9f1f35ad611e0c715639
Meraj Syed <merajhussain1@gmail.com>
2026-03-24 22:08:04 +0530

    stack implementation

---
+++ py/stack.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.items =[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        if len(self.items)==0:
+            raise "Empty stack"
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+    def clear(self):
+        self.items = []
+
+    def __iter__(self):
+        for item in self.items:
+            yield item
+    def to_string(self):
+         return "|".join(map(str,self.items))
---
+++ py/test_stack.py
@@ -0,0 +1,27 @@
+from stack import Stack
+
+
+def test_stack_push():
+    stk = Stack();
+    stk.push(1);
+    stk.push(2);
+    stk.push(3);
+    expected = "1|2|3"
+    assert expected == stk.to_string()
+
+def test_stack_pop():
+    stk = Stack()
+    stk.push(1);
+    stk.push(2);
+    stk.push(3);
+    stk.pop()
+    expected = "1|2"
+    assert expected == stk.to_string()
+
+def test_stack_size():
+    stk = Stack()
+    stk.push(1);
+    stk.push(2);
+    stk.push(3);
+    expected = 3
+    assert expected == stk.size()
