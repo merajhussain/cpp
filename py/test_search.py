966a3e80e6a16182a28f53ddffe554b20f6493a2
Meraj Syed <merajhussain1@gmail.com>
2026-03-31 00:22:57 +0530

    trivial improvement

--- py/test_search.py
+++ py/test_search.py
@@ -33,16 +33,16 @@ def test_binary_search2():
 def test_performace():
     arr = [x for x in range(100000000)]
     s1 = time.perf_counter()
-    linear_search(arr, 100000000)
+    i1=linear_search(arr, 100000000)
 
     e1 = time.perf_counter()
     elapsed1 = e1 - s1
     print()
     print("Linear search elapsed time:",format(elapsed1, '.15f'))
     s2 = time.perf_counter()
-    binary_search(arr, 100000000)
+    i2=binary_search(arr, 100000000)
     e2 = time.perf_counter()
     elapsed2 = e2 - s2
     print("Binary search elapsed:",format(elapsed2, '.15f'))
-    assert elapsed1 > elapsed2
+    assert elapsed1 > elapsed2 and i1==i2
 
