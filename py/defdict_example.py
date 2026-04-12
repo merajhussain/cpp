74293db7d686868f6e6a82128ccf742a80569ee6
Meraj Syed <merajhussain1@gmail.com>
2026-04-12 21:45:23 +0530

    default dict

---
+++ py/defdict_example.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+d=defaultdict(list)
+
+print(d['a'])
+
+d = defaultdict(int)
+print(d['a'])
+d = defaultdict(tuple)
+print(d['a'])
+d = defaultdict(str)
+print(d['a'])
+
+
+
+
