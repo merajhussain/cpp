fff07ea473f3c3ead89fb2aac1f463b22dbdc79d
Meraj Syed <merajhussain1@gmail.com>
2026-04-12 21:50:00 +0530

    default dict example

--- py/defdict_example.py
+++ py/defdict_example.py
@@ -11,6 +11,28 @@ print(d['a'])
 d = defaultdict(str)
 print(d['a'])
 
+names = ["Alice", "Arjun", "Bob", "Basha", "Charlie"]
+
+grouped = {}
+
+for name in names:
+    key = name[0]   # first letter
+
+    if key not in grouped:
+        grouped[key] = []
+
+    grouped[key].append(name)
+
+print(grouped)
 
 
+names = ["Alice", "Arjun", "Bob", "Basha", "Charlie"]
+
+grouped = defaultdict(list)
+
+for name in names:
+    key = name[0]
+    grouped[key].append(name)
+
+print(grouped)
 
