6105fc8df9a173c6971535fee59c3a1317d978d5
Meraj Syed <merajhussain1@gmail.com>
2026-05-14 23:37:59 +0530

    some more itertools examples, filterfalse dropwhile and compress

--- py/finite_iter_tools.py
+++ py/finite_iter_tools.py
@@ -13,3 +13,17 @@ list3=['word1','word2','word3']
 chained_list = chain(list1,list2,list3)
 
 print(list(chained_list))
+
+
+
+
+letters = ['a','b','c']
+bools = [True, False, True]
+compressed_list = compress(letters, bools)
+print(list(compressed_list))
+
+
+
+print (list(dropwhile(lambda x: x<5, [1,4,6,4,1,7])))
+
+print (list(filterfalse(lambda x: x<5, [1,4,6,4,1,7])))
