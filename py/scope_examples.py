699d94f78eb72622f8f0f3286c44aac1aac17a8a
Meraj Syed <merajhussain1@gmail.com>
2026-06-05 15:09:21 +0530

    nonlocal and global variables

---
+++ py/scope_examples.py
@@ -0,0 +1,21 @@
+meraj="either hyderabad/kadapa"
+
+def travel():
+    global meraj
+    meraj="or any beautiful country"
+    print(meraj)
+
+print(meraj)
+travel()
+
+def meraj_nonlocal():
+    meraj="Either Hyderabad/kadapa"
+    print(meraj)
+    def change():
+        nonlocal meraj
+        meraj="or only remote"
+        print(meraj)
+    change()
+
+meraj_nonlocal()
+print("i deserve this for the pain and trauma i have been carrying so far")
