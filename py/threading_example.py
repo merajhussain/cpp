08429a85105dfb60bde3eae7a4740f971e40ed39
Meraj Syed <meraj@cloudaisoft.com>
2026-06-16 13:16:24 +0530

    using locks

--- py/threading_example.py
+++ py/threading_example.py
@@ -13,6 +13,24 @@ t2 = threading.Thread(target=mythread_task("B"))
 t1.start()
 t2.start()
 
+counter =0
+def critical_section_code(task:str):
+    global counter
+    lock = threading.Lock()
+    lock.acquire()
+    counter += 1
+    lock.release()
+    print("Thread {} updated counter value to {}".format(task,counter))
+
+t3 = threading.Thread(target=critical_section_code("task C"))
+t4 = threading.Thread(target=critical_section_code("task D"))
+
+
 t1.join()
 t2.join()
 
+
+t3.start()
+t4.start()
+t3.join()
+t4.join()
